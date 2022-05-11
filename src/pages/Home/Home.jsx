1d7d75fbf89978e3c5967d0c23c2d5a05dec9fc4
Sudeep Mukherjee <sudeep.mukherjee@solvezy.com>
2022-05-11 19:03:24 +0530

    Intial commit - Add topbar components and design

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import Topbar from "../../components/Topbar/Topbar"
+import Sidebar from "../../components/Sidebar/Sidebar"
+import Feed from "../../components/Feed/Feed"
+import Rightbar from "../../components/Rightbar/Rightbar"
+
+import "./home.css"
+
+export default function Home() {
+    return (
+        <>
+            <Topbar />
+            <div className="homeContainer">
+                <Sidebar />
+                <Feed />
+                <Rightbar />
+            </div>
+        </>
+    )
+}
